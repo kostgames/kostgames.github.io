5eb7535de135de54c9568a2e6408f8324dde27bd
kostgames <kostiskyr01@gmail.com>
2016-01-06 16:13:19 +0200

    Update alert.js

--- javascripts/alert.js
+++ javascripts/alert.js
@@ -1,1 +1,8 @@
+/*
+<script src="javascripts/alert.js"></script>
+
+alert("I'll be live at -- Check it out: \nhttp://www.twitch.tv/kost_games ")
+
+*/
+
 alert("I'm live now!   Check it out: \nhttp://www.twitch.tv/kost_games ");
